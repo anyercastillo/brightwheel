e00ecbffbb8c5144503daac52a0dd6ab0daec7e4
Anyer Castillo <anyer.castillo@gmail.com>
2022-01-15 19:24:16 -0500

    Default Android Project

---
+++ app/src/main/java/com/anyer/demo/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.anyer.demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.anyer.demo.ui.theme.DemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DemoTheme {
+        Greeting("Android")
+    }
+}
